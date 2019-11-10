41bbe0e4e8512c071ca6673dd3215252eea9adb4
Daniel Yang <yangafc@bc.edu>
2019-11-10 00:30:04 -0500

    9.11 completed (attempt 2)

--- Snacktacular/SpotsListViewController.swift
+++ Snacktacular/SpotsListViewController.swift
@@ -37,6 +37,7 @@ class SpotsListViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         getLocation()
+        navigationController?.setToolbarHidden(false, animated: false)
         spots.loadData {
             self.sortBasedOnSegmentPressed()
             self.tableView.reloadData()
